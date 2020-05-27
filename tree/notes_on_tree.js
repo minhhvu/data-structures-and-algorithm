0af667ca65a2438060d5831bdfa7f08b190f076b
minhh <minhhaivuhd@gmail.com>
2020-05-27 16:45:38 -0400

    notes on tree

---
+++ tree/notes_on_tree.js
@@ -0,0 +1,27 @@
+/*
+1. Definitions
+  Tree: is a finite set of one or more nodes. There is one specically designated node call the root of tree.
+  The remaining nodes are partitioned into m>0 disjoint set T1, T2, ... and each of these sets turn is a tree.
+  --> Non-linear data structure, no cycle, each node has no more than 2 parents, only one root for one tree
+
+  Search Tree: is a tree which each node n satisfies: all key values on left subtree < n < all key values on right subtree
+
+  Binary Tree: is a tree which each node has at most two child nodes.
+
+  Binary Search Tree: is tree which is a binary tree and is a search tree :D
+
+  Trie
+
+  B-tree
+
+  (a,b)-tree
+
+ */
+
+
+/*
+2. Binary Search Tree
+2.1 Querying BST: searching, minimum, maximum, successor, predecessor
+2.2 Insertion and Deletion
+2.3 Randomly built BST
+ */
